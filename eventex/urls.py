e1da1ac1de2ed620fbe9b972dbfc6005a46a7a78
Amauri <amaurirg@terra.com.br>
2020-11-26 23:28:37 -0200

    SubscriptionForm

--- eventex/urls.py
+++ eventex/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from eventex.core.views import home
+from eventex.subscriptions.views import subscribe
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home)
+    path('', home),
+    path('inscricao/', subscribe),
 ]
