cb789f310b18c997c9d2c3a731a47f828a33bc74
Amauri <amaurirg@terra.com.br>
2020-11-28 17:21:09 -0200

    Message success css

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -15,8 +15,8 @@ def subscribe(request):
             mail.send_mail(
                 'Confirmação de inscrição',
                 body,
-                'contato@eventex.com.br',
-                ['contato@eventex.com.br', form.cleaned_data['email']]
+                form.cleaned_data['email'],
+                [form.cleaned_data['email']]
             )
             messages.success(request, 'Inscricao realizada com sucesso!')
             return HttpResponseRedirect('/inscricao/')
