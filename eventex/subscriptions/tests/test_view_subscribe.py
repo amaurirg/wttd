b9e72004ae57d1deb48fc614c07f8e00a09834b4
Amauri <amaurirg@terra.com.br>
2020-11-30 16:49:56 -0200

    Thank You Page

--- eventex/subscriptions/tests/test_view_subscribe.py
+++ eventex/subscriptions/tests/test_view_subscribe.py
@@ -50,8 +50,8 @@ class SubscribePostValid(TestCase):
         self.resp = self.client.post('/inscricao/', data)
 
     def test_post(self):
-        """Valid POST should redirect to /inscricao/"""
-        self.assertEqual(302, self.resp.status_code)
+        """Valid POST should redirect to /inscricao/1/"""
+        self.assertRedirects(self.resp, '/inscricao/1/')
 
     def test_send_subscribe_email(self):
         """Must send email"""
@@ -82,15 +82,3 @@ class SubscribePostInvalid(TestCase):
 
     def test_dont_save_subscription(self):
         self.assertFalse(Subscription.objects.exists())
-
-
-class SubscribeSuccessMessage(TestCase):
-    def test_message(self):
-        data = {
-            'name': 'Amauri',
-            'cpf': '12345678901',
-            'email': 'contato@eventex.com.br',
-            'phone': '11-99999-8888'
-        }
-        response = self.client.post('/inscricao/', data, follow=True)
-        self.assertContains(response, 'Inscricao realizada com sucesso!')
