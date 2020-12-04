43869351c57d7f1e95fa4863ea83d9490ec39db5
Amauri <amaurirg@terra.com.br>
2020-12-04 12:06:04 -0200

    Speaker in Home

--- eventex/core/tests/test_view_home.py
+++ eventex/core/tests/test_view_home.py
@@ -3,12 +3,13 @@ from django.shortcuts import resolve_url as r
 
 
 class HomeTest(TestCase):
-    """GET / must return status code 200"""
+    fixtures = ['keynotes.json']
+
     def setUp(self):
         self.response = self.client.get(r('home'))
 
-
     def test_get(self):
+        """GET / must return status code 200"""
         self.assertEqual(200, self.response.status_code)
 
     def test_template(self):
@@ -20,8 +21,10 @@ class HomeTest(TestCase):
     def test_speakers(self):
         """Must show keynote speakers"""
         contents = [
+            'href="{}"'.format(r('speaker_detail', slug='grace-hopper')),
             'Grace Hopper',
             'http://hbn.link/hopper-pic',
+            'href="{}"'.format(r('speaker_detail', slug='alan-turing')),
             'Alan Turing',
             'http://hbn.link/turing-pic'
         ]
