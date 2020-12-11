1fb4b8808fafcd54ec9df0c3b65994fb7e711689
Amauri <amaurirg@terra.com.br>
2020-12-11 18:53:21 -0200

    Core Class Based Views

--- eventex/urls.py
+++ eventex/urls.py
@@ -15,8 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from eventex.core.views import home, speaker_detail, talk_list
-
+from eventex.core.views import speaker_detail, talk_list, home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
